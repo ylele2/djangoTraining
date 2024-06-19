dfa8f5a2c8bb3bcb8697a7acb4a923006271f1a4
ylele2 <yasheshlele@hotmail.com>
2024-06-19 20:10:19 +0530

    create views

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", view=views.index)
+    #path("january", view=views.january),
+    #path("february", view=views.february),
+    path("<int:month>",views.monthly_challenge_by_number),
+    path("<str:month>",views.monthly_challenge)
+    
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
-def index(request):
-    return HttpResponse("This Works!")
+'''
+def january(request):
+    return HttpResponse("This is January Month")
 
+def february(request):
+    return HttpResponse("This is February Month")
+'''
+monthly_challenges ={
+    "january": "This is january!!",
+    "february": "This is february!!",
+    "march": "This is march!!",
+    "april": "This is april!!",
+    "may": "This is may!!",
+    "june": "This is June!!",
+    "july": "This is july!!",
+    "august": "This is august!!",
+    "september": "This is september!!",
+    "october": "This is october!!",
+    "november": "This is november!!",
+    "december": "This is december!!"
+
+}
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Month")
+    redirect_month = months[month-1]
+    return HttpResponseRedirect("/challenges/"+redirect_month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This is not a valid month")
+    
